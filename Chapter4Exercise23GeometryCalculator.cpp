2fe4b5631d517af6af28862766f64442628b0ddf
morgan thorne <morgangthorne@outlook.com>
2025-03-02 20:33:00 -0500

    2nd iteration

--- Chapter4Exercise23GeometryCalculator.cpp
+++ Chapter4Exercise23GeometryCalculator.cpp
@@ -44,45 +44,47 @@ int main()
     //Menu
     cout << setw(45) << right << "" << " Geometry Calculator " << endl;
     cout << endl;
+    cout << setw(39) << right << "" << "All answers are rounded to the fifth decimal" << endl;
     cout << setw(39) << right << "" << "1. Calculate the Area of a Circle" << endl;
     cout << setw(39) << right << "" << "2. Calculate the Area of a Rectangle" << endl;
     cout << setw(39) << right << "" << "3. Calculate the Area of a Triangle" << endl;
     cout << setw(39) << right << "" << "4. Quit" << endl;
     cout << setw(39) << right << "" << "Enter your choice (1-4): ";
     cin >> choice;
-
-    //Response to user's choice
-    if (choice == selectcircleArea) {
-        cout << "Enter the radius of the circle: ";
-        cin >> radius;
-        double circleArea = PI * (pow(radius, 2));
-        cout << fixed << setprecision(5) << circleArea;
+    
+    //Response to user's choice if not between 1-4
+    while (choice < 1 || choice > 4) {
+        cout << "Enter a number between 1-4: ";
+        cin >> choice;
     }
-        else if (choice == selectrectangleArea) {
-            cout << "Enter the length of the rectangle: ";
-            cin >> length;
-            cout << "Enter the width of the rectangle: ";
-            cin >> width;
-            double rectangleArea = length * width;
-            cout << fixed << setprecision(5) << rectangleArea;
+    //if and else if for output based off users choice
+        if (choice == selectcircleArea) {
+            cout << "Enter the radius of the circle: ";
+            cin >> radius;
+            double circleArea = PI * (pow(radius, 2));
+            cout << fixed << setprecision(5) << "The Area of the circle is " << circleArea;
+        }
+            else if (choice == selectrectangleArea) {
+                cout << "Enter the length of the rectangle: ";
+                cin >> length;
+                cout << "Enter the width of the rectangle: ";
+                cin >> width;
+                double rectangleArea = length * width;
+                cout << "The Area of the rectangle is " << rectangleArea;
             }
-            else if (choice == selecttriangleArea) {
-                cout << "Enter the base of the triangle: ";
-                cin >> base;
-                cout << "Enter the height of the triangle: ";
-                cin >> height;
-                double triangleArea = base * height * 0.5;
-                cout << fixed << setprecision(2) << triangleArea;
+                else if (choice == selecttriangleArea) {
+                    cout << "Enter the base of the triangle: ";
+                    cin >> base;
+                    cout << "Enter the height of the triangle: ";
+                    cin >> height;
+                    double triangleArea = base * height * 0.5;
+                    cout << "The Area of the triangle is " << triangleArea;
                 }
-                else if (choice == selectQuit) {
-                    cout << "Quitting Program!";
-                    return 0;
-                }
-                    else if (choice < 1 || choice > 4) {
-                        cout << "Please enter a number between 1-4: ";
-                        cin >> choice;
+                    else if (choice == selectQuit) {
+                        cout << "Quitting Program!";    
                     }
-     
+
+
 
 }
 
